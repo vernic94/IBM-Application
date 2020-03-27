c1e7ff861f60eb09dca8033b77622a27f813c2a7
Veronika Nicolaou <vernic@kth.se>
2020-03-27 16:00:56 +0100

    made changes in app.js and mainfest.yml

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(__dirname + '/public'));
   You can export these local json files from IBM Cloud!
 */
 var app_env = cfenv.getAppEnv({vcapFile: 'vcap.json'});
-const IOT_PLATFORM = "Matterhorn";
+const IOT_PLATFORM = "Internet of Things Platform-Vera";
 
 /* Retrieve Cloud Foundry environment variables. */
 var credentials = app_env.getServiceCreds(IOT_PLATFORM);
